d71668b09e45798dcd5041e6eedfa902acf920c0
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-01-22 11:14:02 +0530

    Time: 4 ms (50.19%), Space: 41.8 MB (73.38%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -2,26 +2,26 @@ class Solution {
     public int mostWordsFound(String[] sentences) {
         int max=Integer.MIN_VALUE;
        // String[] arr=sentences.split("\\,");
-        // for(int i=0;i<sentences.length;i++)
-        // {   int c=0;
-        //     String s=sentences[i];
-        //     s=s+" ";
-        //     for(int j=0;j<s.length();j++)
-        //     {
-        //         if(s.charAt(j)==' ')
-        //             c++;
-        //     }
-        //     max=Math.max(max,c);
-        // }
-        // return max;
-        for(int i=0;i<sentences.length;i++)
-        {   int c=0;
-            String s=sentences[i];
-            String[] s1=s.split(" ");
-            c=s1.length;
-         max=Math.max(max,c);
+         for(int i=0;i<sentences.length;i++)
+         {   int c=0;
+             String s=sentences[i];
+             s=s+" ";
+             for(int j=0;j<s.length();j++)
+             {
+                 if(s.charAt(j)==' ')
+                     c++;
+             }
+             max=Math.max(max,c);
+         }
+         return max;
+//         for(int i=0;i<sentences.length;i++)
+//         {   int c=0;
+//             String s=sentences[i];
+//             String[] s1=s.split(" ");
+//             c=s1.length;
+//          max=Math.max(max,c);
             
-        }
-        return max;
+//         }
+//         return max;
     }
 }
