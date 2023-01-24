651e998f04ab1223c223dda27dca88db4b94c7e1
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-01-24 22:20:42 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (61.87%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -3,10 +3,11 @@ class Solution {
         int n=arr.length; int res=0;
         for(int i=0;i<n;i++)
         {
-        int start=n-i;
-        int end=i+1;
-        int total=start*end;
-        int odd=total/2;
+        int start=n-i;   //kitne subarray is index pe jo no.hai usse                            start honge
+            
+        int end=i+1;     //kitne subarray is index pe jo no.hai usse                            end honge
+        int total=start*end;    //given array ke total kitne subarray                                     honge
+        int odd=total/2;        //total me se kitne odd length ke                                      honge
         if(total%2 == 1)
             odd++;
         res+=odd*arr[i];
