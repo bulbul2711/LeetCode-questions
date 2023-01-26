e18be7d076b0736d27bfce47dd332dd517869f59
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-01-26 22:26:18 +0530

    Time: 5 ms (43.03%), Space: 43.1 MB (13.44%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -4,8 +4,8 @@ class Solution {
         String arr[]=s.split(" ");
         for(int i=0;i<k;i++)
         {
-            str+=arr[i]+" ";
-       // str+=" ";
+            str+=arr[i];
+            str+=" ";
     }
         return str.trim();
         
