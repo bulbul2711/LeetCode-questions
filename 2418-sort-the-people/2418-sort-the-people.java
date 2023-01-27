5573b972b2d3340f276990c6f32f7d66617bd365
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-01-28 00:36:55 +0530

    Time: 6 ms (89.32%), Space: 42.6 MB (92.46%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,22 +1,33 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
         
-        for(int i=0;i<heights.length;i++)
-        {
-            for(int j=i+1;j<heights.length;j++)
-                if(heights[i]<heights[j])
-                {
-                    int temp=heights[i];
-                    heights[i]=heights[j];
-                    heights[j]=temp;
+//         for(int i=0;i<heights.length;i++)
+//         {
+//             for(int j=i+1;j<heights.length;j++)
+//                 if(heights[i]<heights[j])
+//                 {
+//                     int temp=heights[i];
+//                     heights[i]=heights[j];
+//                     heights[j]=temp;
                     
-                    String temp1=names[i];
-                    names[i]=names[j];
-                    names[j]=temp1;
-                }
+//                     String temp1=names[i];
+//                     names[i]=names[j];
+//                     names[j]=temp1;
+//                 }
                 
-        }
-        return names;
+//         }
+//         return names;
         
+        //USING HASHMAP
+        HashMap<Integer,String> map=new HashMap<>();
+        for(int i=0;i<heights.length;i++)
+         map.put(heights[i],names[i]);
+        String arr[] = new String[heights.length];
+        Arrays.sort(heights);
+        
+        int index=0;
+        for(int j=heights.length-1;j>=0;j--)
+            arr[index++]=map.get(heights[j]);
+        return arr;
     }
 }
