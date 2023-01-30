e3539b8cddda12ed8ea0df3ee8013a384f1dc830
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-01-31 00:58:16 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (91.79%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,13 +1,27 @@
 class Solution {
     public String toLowerCase(String s) {
-        String str="";
-        for(int i=0;i<s.length();i++)
+        
+//strings are immutable so reassigning it baar baar can cause much garbage collection,so it is time consuming.
+        // String str="";
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     char ch=s.charAt(i);
+        //     if(ch>='A' && ch<='Z')
+        //         ch+=32;
+        //     str+=ch;
+        // }
+        // return str;
+        
+        
+        //using string builder
+        StringBuilder sb=new StringBuilder();
+        char ch[]=s.toCharArray();
+        for(int i=0;i<ch.length;i++)
         {
-            char ch=s.charAt(i);
-            if(ch>='A' && ch<='Z')
-                ch+=32;
-            str+=ch;
+            if(ch[i]>='A' && ch[i]<='Z')
+                ch[i]+=32;
+            sb.append(ch[i]);
         }
-        return str;
+        return sb.toString();
     }
 }
