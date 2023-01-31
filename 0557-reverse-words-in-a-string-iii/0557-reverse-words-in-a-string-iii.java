7e81c6d92f611209e9be6c75771d2fed5cb43538
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-01-31 22:55:52 +0530

    Time: 696 ms (5.60%), Space: 44.7 MB (34.80%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,16 +1,30 @@
 class Solution {
     public String reverseWords(String s) {
         
-        String arr[]=s.split(" ");
-        String res="";
-        for(int i=0;i<arr.length;i++)
-        {
-            String str=arr[i];
-        for(int j=str.length()-1;j>=0;j--)
-            res+=str.charAt(j);
-            res+=" ";
+         String arr[]=s.split(" ");
+         String res="";
+         for(int i=0;i<arr.length;i++)
+         {
+             String str=arr[i];
+         for(int j=str.length()-1;j>=0;j--)
+             res+=str.charAt(j);
+             res+=" ";
 
-        }
-        return res.trim();
-    }
+         }
+         return res.trim();
+        
+        
+        //USING STRING BUFFER
+//         StringBuffer sb=new StringBuffer();
+//          StringBuffer sb1=new StringBuffer();
+//         for(int i=0;i<s.length();i++)
+//             sb.append(s.charAt(i));
+        
+//          for(int i=0;i<sb.length();i++)
+//          {
+//              if(sb.charAt(i)==' ')
+//                  sb.reverse(sb.substring())
+//          }
+//         return sb.toString();
+     }
 }
