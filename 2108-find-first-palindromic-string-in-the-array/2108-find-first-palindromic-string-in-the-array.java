a33519d82ff77312a1425517b953f91841db3554
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-02-03 01:34:37 +0530

    Time: 40 ms (16.67%), Space: 42.2 MB (94.99%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -13,18 +13,13 @@ class Solution {
     
     boolean palindrome(String s)
     {
-       /* StringBuffer sb=new  StringBuffer();
+        StringBuffer sb=new  StringBuffer();
         for(int i=s.length()-1;i>=0;i--)
             sb.append(s.charAt(i));
         if(s.equals(sb.toString()))
             return true;
         else 
-            return false;*/
-        String newstr ="";
-         for(int i=s.length()-1;i>=0;i--)
-             newstr+=s.charAt(i);
-        if(s.equals(newstr))
-            return true;
-        else return false;
+            return false;
+       
     }
 }
