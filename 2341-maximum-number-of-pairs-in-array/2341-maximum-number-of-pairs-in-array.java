8f236ec6ae14460d85fb331b8996a6934ea6ddb9
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-02-05 22:25:59 +0530

    Time: 0 ms (100.00%), Space: 41 MB (67.45%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
@@ -1,21 +1,31 @@
 class Solution {
     public int[] numberOfPairs(int[] nums) {
-       int res[]=new int[2];int c=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(nums[i]==nums[j] &&                               nums[j]!=-1)
-                {   c++; 
-                // nums[i]=-1;
-                 nums[j]=-1;
-                    break;
-                }
-            }
+//        int res[]=new int[2];int c=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             for(int j=i+1;j<nums.length;j++)
+//             {
+//                 if(nums[i]==nums[j] &&                               nums[j]!=-1)
+//                 {   c++; 
+//                  nums[j]=-1;
+//                     break;
+//                 }
+//             }
            
-        }
-        res[0]=c;
-        res[1]=nums.length-(2*c);
-        return res;
+//         }
+//         res[0]=c;
+//         res[1]=nums.length-(2*c);
+//         return res;
+        
+        //USING ONE LOOP ONLY
+        int temp[]=new int[101];int pair=0;int leftover=0;
+         for(int i=0;i<nums.length;i++)
+             temp[nums[i]]++;
+         for(int i=0;i<temp.length;i++)
+         {
+             pair+=temp[i]/2;
+             leftover+=temp[i]%2;
+         }
+        return new int[]{pair,leftover};
 }
 }
