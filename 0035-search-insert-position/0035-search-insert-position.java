657316d5ace176deff77e4da72a5705e2a2d1f57
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-02-27 18:55:08 +0530

    Time: 0 ms (100.00%), Space: 42 MB (44.87%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,6 +1,6 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-       int l=0,r=nums.length;
+    /*   int l=0,r=nums.length;
         while(l<r){
             int mid=l+(r-l)/2;
             if(nums[mid]<target){
@@ -8,6 +8,20 @@ class Solution {
             }
               else
                   r=mid;
+        }
+        return l;*/
+        
+        
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+               return mid;
+            }
+              else if(nums[mid]<target)
+                 l=mid+1;
+            else
+                r=mid-1;
         }
         return l;
     }
