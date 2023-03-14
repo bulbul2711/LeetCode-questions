f11850fa2a57cbbc9d04f853779a2326901bb104
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-03-14 23:14:47 +0530

    Time: 2 ms (99.73%), Space: 44.9 MB (38.67%) - LeetHub

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int i=0,j=1;
+        while(i<nums.length && j<nums.length){
+            if(nums[i]%2==0)
+                i+=2;
+            else if(nums[j]%2!=0)
+                j+=2;
+            else{
+                int t=nums[i];
+                nums[i]=nums[j];
+                nums[j]=t;
+            }
+        }
+        return nums;
+    }
+}
