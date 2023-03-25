311bcedefff4226ffbd4388a58961bd6fcd62a8c
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-03-25 16:47:08 +0530

    Time: 50 ms (51.99%), Space: 65.3 MB (32.13%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         Arrays.sort(nums);
-        for(int i=1;i<nums.length;i+=2){
-            int t=nums[i-1];
-            nums[i-1]=nums[i];
+        for(int i=0;i<nums.length-1;i+=2){
+            int t=nums[i+1];
+            nums[i+1]=nums[i];
             nums[i]=t;
         }
         return nums;
