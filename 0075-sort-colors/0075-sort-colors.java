cbfae72b9574c30f384e754209cf8b8dd7c640a1
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-03-26 16:20:34 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (24.79%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,28 +2,28 @@ class Solution {
     public void sortColors(int[] nums) {
         quick_sort(nums,0,nums.length-1);
     }
-    int[] quick_sort(int[] nums,int left,int right){
+    void quick_sort(int[] nums,int left,int right){
         if(left<right){
             int pos=partition(nums,left,right);
             quick_sort(nums,left,pos-1);
             quick_sort(nums,pos+1,right);
         }
-        return nums;
+      //  return nums;
     }
     int partition(int[] nums,int left,int right){
         int i=left;
-        int j=right;
+        int j=right-1;
         int pivot=nums[right];
-        while(i<j){
-            while(i<j && nums[i]<=pivot)
+        while(i<=j){
+            while(i<=j && nums[i]<=pivot)
                 i++;
-            while(i<j && nums[j]>=pivot)
+            while(i<=j && nums[j]>=pivot)
                 j--;
-           // if(i<j){
+            if(i<j){
                 int temp=nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
-           // }
+            }
         }
         int t=nums[i];
         nums[i]=nums[right];
