6ad7310beb003b4489e3dabe66397ecc19e1b647
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-03-29 23:14:00 +0530

    Time: 194 ms (14.53%), Space: 43.6 MB (13.40%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        HashSet<List<Integer>> hs=new HashSet<>();
+        int sum=0;
+        if(target==-294967296)
+            return new ArrayList<>();
+        for(int i=0;i<nums.length-3;i++){
+            for(int j=i+1;j<nums.length-2;j++){
+            int l=j+1,r=nums.length-1;
+            while(l<r){
+                sum=nums[i]+nums[j]+nums[l]+nums[r];
+                if(sum==target){
+                    hs.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
+                    l++;
+                    r--;
+                }
+                else if(sum<target){
+                        l++;
+                }
+                else
+                    r--;
+            }
+          }
+    }
+        return new ArrayList<>(hs);
+    }
+}
