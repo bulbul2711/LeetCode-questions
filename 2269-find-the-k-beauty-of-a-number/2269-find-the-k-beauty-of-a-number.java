deb3a22c04f48709b19abc744f7b8c4e9c6e1fed
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-04-07 23:16:01 +0530

    Time: 9 ms (6.28%), Space: 40.3 MB (8.68%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -3,13 +3,13 @@ class Solution {
         int c=0,r=k;
         String str=Integer.toString(num);
         for(int i=0;i<=str.length()-k;i++){
-            int x=Integer.parseInt(str.substring(i,r++));
-            if(x==0)
-                continue;
-            else{
-                if(num%x==0)
+            int x=Integer.parseInt(str.substring(i,r));
+          //  r++;
+            System.out.print(x+" ");
+                if(x!=0 && num%x==0)
                     c++;
-            }
+            
+            r++;
         }
         return c;
     }
