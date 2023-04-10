57941212382fe662a665f65899b0c8d9cd327832
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-04-11 00:15:00 +0530

    Time: 1 ms (98.35%), Space: 40.2 MB (84.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-      ArrayList<Character> al=new ArrayList<>();
+     /* ArrayList<Character> al=new ArrayList<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch=='[' || ch=='{' || ch=='(')
@@ -17,6 +17,25 @@ class Solution {
         if((a=='(' && b==')') || (a=='[' && b==']') || (a=='{' && b=='}'))
             return true;
         else
-            return false;
+            return false;*/
+        
+         ArrayList<Character> al=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='[' || ch=='{' || ch=='(')
+                al.add(ch);
+            else if(al.isEmpty())
+                return false;
+            else if(ch==')' && al.get(al.size()-1)=='(')
+                 al.remove(al.size()-1);
+            else if(ch=='}' && al.get(al.size()-1)=='{')
+                 al.remove(al.size()-1);
+            else if(ch==']' && al.get(al.size()-1)=='[')
+                 al.remove(al.size()-1);
+            else 
+                return false;
+            
+        }
+        return (al.isEmpty());
     }
 }
