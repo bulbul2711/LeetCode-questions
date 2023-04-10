17f31cd29fdbf5b1bdd54d2574242f10fac5c478
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-04-10 23:57:26 +0530

    Time: 1 ms (98.35%), Space: 40.6 MB (42.60%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean isValid(String s) {
       ArrayList<Character> al=new ArrayList<>();
-        int c=-1;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch=='[' || ch=='{' || ch=='('){
+            if(ch=='[' || ch=='{' || ch=='(')
                 al.add(ch);
-                c++;
-            }
-            else if(!al.isEmpty() && matching(al.get(c),ch)){
-                al.remove(c);
-                c--;
-            }
-            else
+            else if(!al.isEmpty() && matching(al.get(al.size()-1),ch))
+                al.remove(al.size()-1);
+             else
                 return false;
         }
         return (al.isEmpty());
