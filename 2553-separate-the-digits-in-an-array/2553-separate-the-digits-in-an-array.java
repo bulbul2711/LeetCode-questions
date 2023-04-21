8287eb528a528a57c021528d7434cdf48e73cf5f
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-04-22 00:47:12 +0530

    Time: 4 ms (90.60%), Space: 43.2 MB (69.98%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -1,7 +1,5 @@
 class Solution {
     public int[] separateDigits(int[] nums) {
-       
-       // ArrayList<Integer> al=new ArrayList<>();
         StringBuffer sb=new StringBuffer();
         for(int i=0;i<nums.length;i++)
         {
@@ -9,7 +7,7 @@ class Solution {
         }
          int ans[]=new int[sb.length()];
         for(int i=0;i<sb.length();i++)
-            ans[i]=(int)(sb.charAt(i))-48;
+            ans[i]=(sb.charAt(i))-'0';
         return ans;
     }
 }
