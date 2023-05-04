07051edce69ee26f0a05ecda1f8da7af236e06b6
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-05-04 23:16:35 +0530

    Time: 19 ms (72.01%), Space: 44.1 MB (29.40%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -3,14 +3,11 @@ class Solution {
         int max=0;
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-           if(map.containsKey(nums[i]))
-               map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            else
-                map.put(nums[i],1);
+             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i]) && map.containsKey(nums[i]+1))
-                max=Math.max(max,map.get(nums[i])+map.get(nums[i]+1));
+        for(int i:map.keySet()){            //map.values()
+            if(map.containsKey(i+1))
+                max=Math.max(max,map.get(i)+map.get(i+1));
         }
         return max;
     }
