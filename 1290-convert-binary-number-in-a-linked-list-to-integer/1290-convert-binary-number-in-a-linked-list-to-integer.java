620d83cad272896dfc8a4a6d202ef1b88df7a162
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-05-12 23:14:53 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (16.24%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -10,15 +10,20 @@
  */
 class Solution {
     public int getDecimalValue(ListNode head) {
-        ListNode curr=head;int c=0;
+       // ListNode curr=head;int c=0;
         int sum=0;
-        while(curr!=null){
+       /* while(curr!=null){
             c++;
             curr=curr.next;
         }
         c=c-1;
         while(head!=null){
             sum+=(Math.pow(2,c--)*head.val);
+            head=head.next;
+        }
+        return sum;*/
+        while(head!=null){
+            sum=sum*2+head.val;
             head=head.next;
         }
         return sum;
