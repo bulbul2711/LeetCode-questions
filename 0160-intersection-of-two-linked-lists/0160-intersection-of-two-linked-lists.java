8c332d925e20461225fc1e73e92cf5dcc91877f4
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-05-17 23:52:32 +0530

    Time: 1 ms (99.72%), Space: 45.4 MB (45.77%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,8 +11,40 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-      //  count the number of nodes to be skipped in bigger linkedlist
+    /*  if(head==null || head)
         int c1=0,c2=0;
+        ListNode curr1=headA,curr2=headB;
+        while(curr1!=null){
+            c1++;
+            curr1=curr1.next;
+        }
+         while(curr2!=null){
+            c2++;
+            curr2=curr2.next;
+        }
+        int diff=Math.abs(c1-c2);
+        if(c1>c2){
+            while(diff!=0){
+                headA=headA.next;
+                diff--;
+            }
+        }
+        else{
+            while(diff!=0){
+                headB=headB.next;
+                diff--;
+            }
+        }
+        while(headA!=null){
+            if(headA.next==headB.next)
+                return headA.next;
+            else{
+                headA=headA.next;
+                headB=headB.next;
+            }
+        }
+        return null;*/
+         int c1=0,c2=0;
         if(headA==null || headB==null)
              return null;
         for(ListNode curr=headA;curr!=null;curr=curr.next)
@@ -28,7 +60,7 @@ public class Solution {
             count--;
         }
          
-         while(headA!=null){
+         while(headA!=null && headB!=null){
              if(headA==headB)
                  return headA;
              headA=headA.next;
@@ -36,6 +68,5 @@ public class Solution {
          }
          return null;
         
-      
     }
 }
