78acf981ef8e35f6a8e649cac7de57433a1a7374
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-05-17 22:27:21 +0530

    Time: 4 ms (85.76%), Space: 63 MB (49.81%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,43 +10,30 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        
-        //TC:o(n), AS:O(n)
-   /*     ListNode head1=head;
-      ArrayDeque<Integer> stack=new ArrayDeque<>();
-        for(ListNode curr=head;curr!=null;curr=curr.next){
-            stack.push(curr.val);
-        }
-        for(ListNode curr=head1;curr!=null;curr=curr.next){
-            if(curr.val!=stack.pop())
-                return false;
-        }
-        return true;*/
-        if(head==null || head.next==null) return true;
-        ListNode slow=head,fast=head,curr=head;
-        while(fast!=null && fast.next!=null && fast.next.next!=null){
+       ListNode slow=head,fast=head;
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
-        ListNode headOfreversed=reverse(slow.next);
-        while(headOfreversed!=null){
-            if(curr.val!=headOfreversed.val)
-                return false;
-            curr=curr.next;
-            headOfreversed=headOfreversed.next;
-        }
-        return true;
+       ListNode newhead= reverse(slow);
+    while(newhead!=null){
+        if(newhead.val!=head.val)
+            return false;
+        head=head.next;
+        newhead=newhead.next;
     }
-        ListNode reverse(ListNode head1){
-            if(head1==null || head1.next==null)
-                return head1;
-            ListNode prev=null,curr1=head1;
-            while(curr1!=null){
-                ListNode next=curr1.next;
-                curr1.next=prev;
-                prev=curr1;
-                curr1=next;
-             }
-            return prev;
-        }
+        return true;
+}
+    ListNode reverse(ListNode head1){
+         ListNode curr=head1;
+         ListNode prev=null;
+         ListNode Next=curr.next;
+         while(curr!=null){
+             Next=curr.next;
+             curr.next=prev;
+             prev=curr;
+             curr=Next;
+         }
+         return prev;
+     }
 }
