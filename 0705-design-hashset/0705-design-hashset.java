fb2d9997d9d491951e353be3bc156d028c4099a1
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-01 00:29:14 +0530

    Time: 17 ms (66.21%), Space: 51.3 MB (11.63%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,12 +1,12 @@
 class MyHashSet {
     int size=1000;
-    ArrayList<Integer>[] a;
+    ArrayList<Integer>[] a;        //array of arraylist
     public MyHashSet() {
         a=new ArrayList[size];
     }
     public void add(int key) {
         int h=key%size;
-        if(a[h]==null)
+        if(a[h]==null)                   //default value of arraylist is null
             a[h]=new ArrayList<>();
         if(!a[h].contains(key))
             a[h].add(key);
@@ -14,7 +14,7 @@ class MyHashSet {
     public void remove(int key) {
         int h=key%size;
         if(a[h]!=null)
-            a[h].remove(Integer.valueOf(key));
+            a[h].remove(Integer.valueOf(key));   //remove function will not throw error if the value to be removed is not in the list
     }
     
     public boolean contains(int key) {
