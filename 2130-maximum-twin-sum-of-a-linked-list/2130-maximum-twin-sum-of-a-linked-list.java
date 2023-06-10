a74979e7fa818ab195cf43f2b23f840cba0c3a1a
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-10 23:26:39 +0530

    Time: 7 ms (54.04%), Space: 65.8 MB (27.16%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -28,25 +28,44 @@ class Solution {
         // }
         // return max;
         
-        ArrayDeque<Integer> st=new ArrayDeque<>();
-        ListNode curr=head;
-        int c=0,sum=0;
-        int max=Integer.MIN_VALUE;
-        while(curr!=null){
-            c++;
-            curr=curr.next;
-        }
-        curr=head;
-        for(int i=0;i<c/2;i++){
-            st.push(curr.val);
-            curr=curr.next;
-        }
-        while(curr!=null){
-            sum+=st.pop()+curr.val;
-            max=Math.max(sum,max);
-            curr=curr.next;
-            sum=0;
+        //find the middle
+        ListNode middle = findMiddle(head);
+
+        //reverse second half
+        ListNode second = reverse(middle);
+
+        //find pair sum max
+        int max = 0;
+        while (head != null && second != null) {
+
+            max = Math.max(head.val + second.val, max);
+            head = head.next;
+            second = second.next;
         }
         return max;
+    }
+
+    public ListNode findMiddle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverse(ListNode head) {
+
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
     }
 }
