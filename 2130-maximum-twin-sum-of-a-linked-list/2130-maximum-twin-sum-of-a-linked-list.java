b65798032c1d505617af02b7f9a37e4b4de9d8ef
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-10 23:28:06 +0530

    Time: 5 ms (85.12%), Space: 63 MB (46.29%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,61 +10,51 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        // ListNode curr=head;
-        // ArrayList<Integer> al=new ArrayList<>();
-        // while(curr!=null){
-        //     al.add(curr.val);
-        //     curr=curr.next;
-        // }
-        // int l=0,r=al.size()-1;
-        // int sum=0;
-        // int max=Integer.MIN_VALUE;
-        // while(l<r){
-        //     sum+=al.get(l)+al.get(r);
-        //     max=Math.max(sum,max);
-        //     sum=0;
-        //     l++;
-        //     r--;
-        // }
-        // return max;
+        int max=Integer.MIN_VALUE;
+      /*  if(head==null) return 0;
+        if(head.next.next==null) return head.val+head.next.val;
+        else{
+        ListNode curr=head;
+        ArrayList<Integer> al =new ArrayList<>();
+        while(curr!=null){
+            al.add(curr.val);
+            curr=curr.next;
+        }
+        int n=al.size();int sum=0;
         
-        //find the middle
-        ListNode middle = findMiddle(head);
-
-        //reverse second half
-        ListNode second = reverse(middle);
-
-        //find pair sum max
-        int max = 0;
-        while (head != null && second != null) {
-
-            max = Math.max(head.val + second.val, max);
-            head = head.next;
-            second = second.next;
+        for(int i=0;i<n/2;i++){
+            sum+=al.get(i)+al.get(n-i-1);
+            max=Math.max(max,sum);
+            sum=0;
+        }
+        }
+        return max;*/
+        if(head==null) return 0;
+        if(head.next.next==null) return head.val+head.next.val;
+        int sum=0;
+        ListNode slow=head,fast=head,curr=head;
+          while(fast!=null && fast.next!=null)  {
+              slow=slow.next;
+              fast=fast.next.next;
+          }   
+        ListNode head1=reverse(slow);
+        while(head1!=null){
+             sum=0;
+            sum=head1.val+curr.val;
+            max=Math.max(max,sum);
+            head1=head1.next;
+            curr=curr.next;
+           
         }
         return max;
     }
-
-    public ListNode findMiddle(ListNode head) {
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
-    public ListNode reverse(ListNode head) {
-
-        ListNode prev = null;
-        while (head != null) {
-            ListNode temp = head.next;
-            head.next = prev;
-            prev = head;
-            head = temp;
+    ListNode reverse(ListNode mid_head){
+        ListNode curr=mid_head,prev=null;
+        while(curr!=null){
+            ListNode Next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=Next;
         }
         return prev;
     }
