bd5695dadb4b25b7403819fcb0a7a2d797b5ace1
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-10 23:12:26 +0530

    Time: 14 ms (44.59%), Space: 58 MB (95.50%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,53 +10,22 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        int max=Integer.MIN_VALUE;
-      /*  if(head==null) return 0;
-        if(head.next.next==null) return head.val+head.next.val;
-        else{
         ListNode curr=head;
-        ArrayList<Integer> al =new ArrayList<>();
+        ArrayList<Integer> al=new ArrayList<>();
         while(curr!=null){
             al.add(curr.val);
             curr=curr.next;
         }
-        int n=al.size();int sum=0;
-        
-        for(int i=0;i<n/2;i++){
-            sum+=al.get(i)+al.get(n-i-1);
-            max=Math.max(max,sum);
-            sum=0;
-        }
-        }
-        return max;*/
-        if(head==null) return 0;
-        if(head.next.next==null) return head.val+head.next.val;
+        int l=0,r=al.size()-1;
         int sum=0;
-        ListNode slow=head,fast=head,curr=head;
-          while(fast!=null && fast.next!=null)  {
-              slow=slow.next;
-              fast=fast.next.next;
-          }   
-        ListNode head1=reverse(slow);
-        System.out.print(head1.val+","+head1.next.val);
-        while(head1!=null){
-             sum=0;
-            sum=head1.val+curr.val;
-            max=Math.max(max,sum);
-            head1=head1.next;
-            curr=curr.next;
-           
+        int max=Integer.MIN_VALUE;
+        while(l<r){
+            sum+=al.get(l)+al.get(r);
+            max=Math.max(sum,max);
+            sum=0;
+            l++;
+            r--;
         }
         return max;
-    }
-    ListNode reverse(ListNode mid_head){
-        ListNode curr=mid_head,prev=null;
-        while(curr!=null){
-            ListNode Next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=Next;
-        }
-        return prev;
     }
 }
