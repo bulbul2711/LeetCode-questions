1dc61db39b65d0466389adb83cf2cb287fd365d2
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-13 23:34:53 +0530

    Time: 125 ms (15.25%), Space: 47.8 MB (89.14%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -3,25 +3,29 @@ class Solution {
        
         HashMap<String,Integer> map=new HashMap<>();
         int c=0;
-        
+         
         for(int i=0;i<grid.length;i++){
-             String sb="";
+            StringBuffer sb=new StringBuffer();
             for(int j=0;j<grid.length;j++){
-                sb+=grid[i][j];
-                 sb+=",";
+                sb.append(grid[i][j]);
+                 sb.append(",");
             }
-            map.put(sb,map.getOrDefault(sb,0)+1);
-          
+            System.out.println(sb);
+            map.put(sb.toString(),map.getOrDefault(sb.toString(),0)+1);
+           // sb.delete(0,sb.length());
         }
         
         for(int k=0;k<grid.length;k++){
-            String sb1="";
+            StringBuffer sb1=new StringBuffer();
             for(int l=0;l<grid.length;l++){
-                sb1+=grid[l][k];
-                 sb1+=",";
+                sb1.append(grid[l][k]);
+                 sb1.append(",");
             }
-            if(map.containsKey(sb1))
-                c+=map.get(sb1);
+              System.out.println(sb1);
+            if(map.containsKey(sb1.toString()))
+                c+=map.get(sb1.toString());
+              System.out.println(c);
+              // sb1.delete(0,sb1.length());
         }
         return c;
     }
