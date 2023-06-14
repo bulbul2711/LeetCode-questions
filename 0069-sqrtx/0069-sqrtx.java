fb48181e7a149e7fc71fb8f8247abb1f8347e693
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-14 11:50:16 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (66.91%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,25 +1,8 @@
 class Solution {
     public int mySqrt(int x) {
        
-//         if(x==1 || x==0) return x;
-//         int low=1,high=x,ans=0;
-//         while(low<=high){
-//             int mid=low+(high-low)/2;
-//             if(mid==x/mid){
-//                 ans= mid;
-//                 break;
-//             }
-//             else if(mid<x/mid){
-//                 low=mid+1;
-//                 ans=mid;
-//             }
-//             else
-//                 high=mid-1;
-//         }
-//         return ans;
-        
-           if(x==0) return 0;
-   int low=1,high=x,ans=0;
+        //if(x==0) return x;
+        int low=1,high=x,ans=0;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(mid==x/mid){
@@ -34,6 +17,8 @@ class Solution {
                 high=mid-1;
         }
         return ans;
+        
+    
         
     }
 }
