f79fcaf3430b6d7d5af387776386842449ad614c
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-25 23:58:22 +0530

    Time: 1 ms (100.00%), Space: 43.6 MB (61.87%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -3,17 +3,17 @@ class Solution {
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> ds=new ArrayList<>();
         Arrays.sort(nums);
-        possible(nums,ds,res);
+        dfs(nums,ds,res);
         return res;
     }
-    public void possible(int[] nums,List<Integer> ds,List<List<Integer>> res){
-          res.add(ds);
+    public void dfs(int[] nums,List<Integer> ds,List<List<Integer>> res){
+            res.add(ds);
         for(int i=0;i<nums.length;i++){
             if(i>0 && nums[i]==nums[i-1])
                 continue;
             List<Integer> al=new ArrayList<>(ds);
             al.add(nums[i]);
-            possible(Arrays.copyOfRange(nums,i+1,nums.length),al,res);
+            dfs(Arrays.copyOfRange(nums,i+1,nums.length),al,res);
         }
     }
 }
