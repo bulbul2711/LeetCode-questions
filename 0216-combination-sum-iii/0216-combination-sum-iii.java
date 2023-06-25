5342b2513c29afbd266fc1b9316d6e054e83abc4
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-26 00:35:06 +0530

    Time: 1 ms (36.72%), Space: 40.7 MB (6.63%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> ds=new ArrayList<>();
+        int[] a={1,2,3,4,5,6,7,8,9};
+        dfs(a,n,k,ds,res);
+        return res;
+    }
+    public void dfs(int[] a,int n,int k,List<Integer> ds,List<List<Integer>> res){
+        if(k<0 || n<0)
+            return;
+        if(n==0 && k==0){
+            res.add(ds);
+            return ;
+        }
+        for(int i=0;i<a.length;i++){
+            List<Integer> al=new ArrayList<>(ds);
+            al.add(a[i]);
+            dfs(Arrays.copyOfRange(a,i+1,a.length),n-a[i],k-1,al,res);
+        }
+    }
+}
