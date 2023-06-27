822e8b72e0a4e592374c094ad71710781f37c5b5
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-27 23:55:58 +0530

    Time: 1 ms (98.91%), Space: 44.1 MB (20.73%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> ds=new ArrayList<>();
+        dfs(nums,ds,res);
+        return res;
+    }
+    public void dfs(int[] nums,List<Integer> ds,List<List<Integer>> res){
+        if(nums.length==0){
+            res.add(ds);
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            List<Integer> al=new ArrayList<>(ds);
+            al.add(nums[i]);
+            dfs(remove(nums,i),al,res);
+        }
+    }
+    public int[] remove(int[] nums,int index){
+        int[] a=new int[nums.length-1];
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(i!=index)
+                a[j++]=nums[i];
+        }
+        return a;
+    }
+}
