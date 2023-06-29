9474305efb9970c6c2e5dac6d54d9cf0c0ca3517
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-30 00:27:20 +0530

    Time: 7 ms (32.02%), Space: 44.1 MB (7.76%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res=new  ArrayList<String>();
+        String path="";
+        if(s.length()>12)
+            return res;
+        dfs(s,0,path,res);
+        return res;
+    }
+    public void dfs(String s, int dots, String path, List<String> res){
+        if(dots==4 && s.length()==0){
+            res.add(path.substring(0,path.length()-1));
+            return;
+        }
+        for(int i=1;i<4;i++){
+            if(i>s.length())
+                break;
+            if(i>1 && s.charAt(0)=='0')
+                continue;
+            if(i>2 && Integer.parseInt(s.substring(0,i))>255)
+                continue;
+            dfs(s.substring(i), dots+1, path+s.substring(0,i)+".", res);
+        }
+    }
+}
