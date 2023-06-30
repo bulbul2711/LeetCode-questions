5b796171c569707d0f47735f135ed53918ccfaf3
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-06-30 20:11:11 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (96.33%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,32 +1,31 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int l=nums[0],r=0;
-        for(int i=0;i<nums.length;i++)
+        int l=Integer.MIN_VALUE;
+        int r=0;
+        for(int i=0;i<nums.length;i++){
             l=Math.max(l,nums[i]);
-         for(int i=0;i<nums.length;i++)
-             r+=nums[i];
+            r+=nums[i];
+        }
         while(l<r){
-            int mid=l+(r-l)/2;
-            if(feasible(mid,nums,k))
-                r=mid;
+            int m=l+(r-l)/2;
+            if(feasible(m,nums,k))
+                r=m;
             else
-                l=mid+1;
+                l=m+1;
         }
         return l;
     }
-    
-    boolean feasible(int mid,int[] nums,int k){
-        int total=0,part=1;
-         for(int i=0;i<nums.length;i++){
-             total+=nums[i];
-             if(total>mid){
-                 total=nums[i];
-                 part+=1;
-                 if(part>k)
-                     return false;
-             }
-                 
-         }
+    public boolean feasible(int m,int[] nums,int k){
+        int total=0,parts=1;
+        for(int i=0;i<nums.length;i++){
+            total+=nums[i];
+            if(total>m){
+                total=nums[i];
+                parts++;
+                if(parts>k)
+                    return false;
+            }
+        }
         return true;
     }
 }
