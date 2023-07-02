f6f9375baf2c53001b82026b77490ce112a62958
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-02 10:50:33 +0530

    Time: 5 ms (80.68%), Space: 43.5 MB (53.22%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -23,15 +23,15 @@ class Solution {
         // }
         // return s;
         
-        String s="";
+        StringBuilder s=new StringBuilder();
         int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         for(int i=0;i<values.length;i++){
             while(num>=values[i]){
-                s+=strs[i];
+                s.append(strs[i]);
                 num-=values[i];
             }
         }
-        return s;
+        return s.toString();
     }
 }
