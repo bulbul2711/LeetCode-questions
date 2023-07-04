4b9f021c9d6df570c520bd5c40f67afc5128f139
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-04 17:06:07 +0530

    Time: 2 ms (80.98%), Space: 43.7 MB (65.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,18 +3,16 @@ class Solution {
         int res[]=new int[2];
         Arrays.fill(res,-1);
         Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++)
-        {
+        for(int i=0;i<nums.length;i++){
             int x=target-nums[i];
-            if(map.containsKey(x))
-            { res[0]=map.get(x);
-            res[1]=i;
-             break;
+            if(map.containsKey(x)){
+                res[0]=map.get(x);
+                res[1]=i;
+                break;
             }
             else
-            map.put(nums[i],i);
+                map.put(nums[i],i);
         }
         return res;
-        
-}
+    }
 }
