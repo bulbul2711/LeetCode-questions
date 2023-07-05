cdbcfb34d9e2a9c225ace736e017f872750275ea
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-05 14:50:14 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (60.68%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        //this solution will work for every case like(unique elements,two same elements,three same elements)
        int j=2;
         for(int i=2;i<nums.length;i++){
             if(nums[i]!=nums[j-2]){
