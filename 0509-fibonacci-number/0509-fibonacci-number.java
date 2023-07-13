fafbb87df538c876198cdffdccdf61dd44f247e4
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-13 23:56:29 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (58.25%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -5,6 +5,8 @@ class Solution {
        //  if(n==1)
        //      return 1;
        //  return fib(n-1)+fib(n-2);
+     
+ /*  =========recusive = 10ms , iterative = 0ms=============   */  
         if(n==0) return 0;
         if(n==1) return 1;
         int a=0,b=1,c=0;
@@ -14,6 +16,6 @@ class Solution {
             a=b;
             b=c;
         }
-       return c;
+       return c;  
     }
 }
