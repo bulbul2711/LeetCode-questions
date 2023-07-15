51f80e0506180276788f6ae0636b0b79e0a10804
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-16 00:30:13 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (57.30%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,22 @@
 class Solution {
     public int fib(int n) {
+        /* MEMOIZATION */
+    //     int dp[]=new int[n+1];
+    //     return fibo(n,dp);
+    // }
+    // int fibo(int n,int[] dp){
+    //     if(n<=1) return n;
+    //     else if(dp[n]!=0) return dp[n];
+    //     dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+    //     return dp[n];
+        
+        /* TABULATION */
+        if(n==0) return 0;
         int dp[]=new int[n+1];
-        return fibo(n,dp);
-    }
-    int fibo(int n,int[] dp){
-        if(n<=1) return n;
-        else if(dp[n]!=0) return dp[n];
-        return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
-       // return dp[n];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 }
