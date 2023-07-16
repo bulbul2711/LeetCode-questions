7aa471e003b5843fc0f18414a13c83882006a8d4
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-16 23:39:18 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (94.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public int fib(int n) {
-        /* MEMOIZATION */
-    //     int dp[]=new int[n+1];
-    //     return fibo(n,dp);
-    // }
-    // int fibo(int n,int[] dp){
-    //     if(n<=1) return n;
-    //     else if(dp[n]!=0) return dp[n];
-    //     dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
-    //     return dp[n];
+    /* MEMOIZATION */
+        //     int dp[]=new int[n+1];
+        //     return fibo(n,dp);
+        // }
+        // int fibo(int n,int[] dp){
+        //     if(n<=1) return n;
+        //     else if(dp[n]!=0) return dp[n];
+        //     dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+        //     return dp[n];
         
-        /* TABULATION */
+    /* TABULATION */
         // if(n==0) return 0;
         // int dp[]=new int[n+1];
         // dp[0]=0;
@@ -19,7 +19,7 @@ class Solution {
         //     dp[i]=dp[i-1]+dp[i-2];
         // return dp[n];
         
-        /* Space optimization */
+    /* SPACE OPTIMIZATION */
         if(n==0) return 0;
         else if(n==1) return 1;
         int a=0,b=1,c=0;
