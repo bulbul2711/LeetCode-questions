af6dab1a94183bc99ccbc573bf0a76f72a0bc754
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-23 00:47:36 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (33.90%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,35 +1,27 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        ArrayList<Integer> al=new ArrayList<>();
-        int n=digits.length; int c=0;
-        int x=digits[n-1]+1;
-        if(x>9){ 
-            al.add(0);
+        int n=digits.length; 
+       int[] res=new int[n+1];
+        int c=0;int i=n;
+        res[i]=digits[n-1]+1;
+        if(res[i]>9){ 
+            res[i]=0;
             c=1;
         }
-        else
-            al.add(x);
-        for(int i=digits.length-2;i>=0;i--){
-            if(c>0){
-                if(digits[i]+c>9){
-                    al.add(0);
-                    c=1;
-                }
-                else{
-                    al.add(digits[i]+c);
-                    c=0;
-                }
+        for(int j=n-2;j>=0;j--){
+            res[--i]=digits[j]+c;
+            if(res[i]>9){
+                res[i]=0;
+                c=1;
             }
-            else
-                 al.add(digits[i]);
-            
+            else{
+                c=0;
+            }
         }
-        if(c==1)
-            al.add(1);
-        Collections.reverse(al);
-        int[] res=new int[al.size()];
-        for(int i=0;i<res.length;i++)
-            res[i]=al.get(i);
-        return res;
+        if(c==1){
+            res[--i]=1;
+            return res;
+        }
+        return Arrays.copyOfRange(res,1,res.length);
     }
 }
