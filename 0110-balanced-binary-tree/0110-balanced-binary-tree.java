3ded41feae02f1f9c212257d7f423b1ed8d5dea7
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-22 21:40:05 +0530

    Time: 1 ms (92.62%), Space: 43.3 MB (50.67%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -27,7 +27,7 @@ class Solution {
         int rh=checkHeight(root.right);
         if(lh==-1 || rh==-1)
             return -1;
-        if(Math.abs(lh-rh)>1)
+        else if(Math.abs(lh-rh)>1)
             return -1;
         return Math.max(lh,rh)+1;
     }
