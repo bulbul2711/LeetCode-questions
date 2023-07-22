9a5be1a35ff2b10629425d9a41e2673a3d277850
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-23 01:07:40 +0530

    Time: 0 ms (100.00%), Space: 41 MB (67.62%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,27 +1,14 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n=digits.length; 
-       int[] res=new int[n+1];
-        int c=0;int i=n;
-        res[i]=digits[n-1]+1;
-        if(res[i]>9){ 
-            res[i]=0;
-            c=1;
-        }
-        for(int j=n-2;j>=0;j--){
-            res[--i]=digits[j]+c;
-            if(res[i]>9){
-                res[i]=0;
-                c=1;
-            }
-            else{
-                c=0;
-            }
-        }
-        if(c==1){
-            res[--i]=1;
-            return res;
-        }
-        return Arrays.copyOfRange(res,1,res.length);
+       for(int i=digits.length-1;i>=0;i--){
+           if(digits[i]<9){
+               digits[i]++;
+               return digits;
+           }
+           digits[i]=0;
+       }
+        int res[]=new int[digits.length+1];
+        res[0]=1;
+        return res;
     }
 }
