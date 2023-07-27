2e31c801823e2e4ded101fb7732c1d70d5d76ed8
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-27 15:12:25 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (42.46%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,34 +13,18 @@
  *     }
  * }
  */
-class pair{
-    TreeNode node;
-    int level;
-    pair(TreeNode node,int level){
-        this.node=node;
-        this.level=level;
-    }
-}
 class Solution {
+     List<Integer> res=new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {
-        ArrayList<Integer> res=new ArrayList<>();
-        if(root==null) return res;
-        Queue<pair> q=new ArrayDeque<>();        //(Node,level)
-        Map<Integer,Integer> map=new TreeMap<>(); //(level,Node.data)
-        q.offer(new pair(root,0));
-        while(!q.isEmpty()){
-            pair p=q.poll();
-            TreeNode temp=p.node;
-            int level=p.level;
-            if(!map.containsKey(level))
-               map.put(level,temp.val);
-            if(temp.right!=null)
-               q.offer(new pair(temp.right,level+1));
-            if(temp.left!=null)
-               q.offer(new pair(temp.left,level+1));
-        }
-        for(Map.Entry<Integer,Integer> entry: map.entrySet())
-           res.add(entry.getValue());
-        return res;
+        dfs(root,0);
+            return res;
+    }
+    void dfs(TreeNode root,int level){
+        if(root==null)
+            return;
+        if(res.size()==level)
+            res.add(root.val);
+        dfs(root.right,level+1);
+        dfs(root.left,level+1);
     }
 }
