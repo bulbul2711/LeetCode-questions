b422b66eb31d5c9b284d8839dc1fcc10c2242dae
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-07-28 01:09:51 +0530

    Time: 23 ms (52.68%), Space: 57.7 MB (6.25%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -5,16 +5,11 @@ class Solution {
         Arrays.sort(batteries);
         for(int i=0;i<numOfBat;i++)
             sum+=batteries[i];
-       // ArrayList<Integer> list=new A
         while(n>1 && batteries[numOfBat-1]>sum/n){
             n--;
             sum-=batteries[numOfBat-1];
-            batteries[numOfBat-1]=0;
             numOfBat--;
         }
-        // if(n==0)
-        //     return batteries[0];
-        // else
         return sum/n;
     }
 }
