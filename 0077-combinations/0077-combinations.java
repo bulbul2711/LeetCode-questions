bf4ff2cf6d54d3c52b1f0e693f89f17634d757f7
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-08-01 23:57:18 +0530

    Time: 48 ms (11.19%), Space: 44.6 MB (99.62%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res=new ArrayList<>();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=i+1;
+        List<Integer> path=new ArrayList<>();
+        dfs(a,k,path,res);
+        return res;
+    }
+    void dfs(int[]a, int k, List<Integer> path, List<List<Integer>> res){
+        if(path.size()==k){
+            res.add(path);
+            return;
+        }
+        for(int i=0;i<a.length;i++){
+            if(path.size()>k)
+                continue;
+            List<Integer> al=new ArrayList<>(path);
+            al.add(a[i]);
+            dfs(Arrays.copyOfRange(a,i+1,a.length), k, al, res);
+        }
+    }
+}
