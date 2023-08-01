61fb9ae57190ca8de46080a80631788c35f050f5
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-08-02 00:14:46 +0530

    Time: 46 ms (11.51%), Space: 44.7 MB (99.08%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -9,14 +9,11 @@ class Solution {
         return res;
     }
     void dfs(int[]a, int k, List<Integer> path, List<List<Integer>> res){
-        // if(path.size()>k)
-        //     return;
         if(path.size()==k){
             res.add(path);
             return;
         }
         for(int i=0;i<a.length;i++){
-            
             List<Integer> al=new ArrayList<>(path);
             al.add(a[i]);
             dfs(Arrays.copyOfRange(a,i+1,a.length), k, al, res);
