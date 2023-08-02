ad248f209595672bdaa3dae7c5b087151fa4792b
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-08-03 00:08:59 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (97.53%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] dp) {
+        int m=dp.length;
+        int n=dp[0].length;
+         if(dp[m-1][n-1]==1)
+             return 0;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1)
+                    dp[i][j]=1;
+                else if(dp[i][j]==1)
+                    dp[i][j]=0;
+                else if(i==m-1)
+                    dp[i][j]=dp[i][j+1];
+                else if(j==n-1)
+                     dp[i][j]=dp[i+1][j];
+                else
+                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+}
