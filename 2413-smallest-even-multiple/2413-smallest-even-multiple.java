7a055d27fce846664ed56b6d70a3f314acf4a387
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-08-03 23:52:14 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (57.19%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,10 +1,8 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        int ans=2;
-        for(int i=n;i<=2*n;i+=n){
-            if(i%2==0 && i%n==0)
-                return i;
-        }
-        return 2*n;
+        if(n%2==0)
+            return n;
+        else
+            return 2*n;
     }
 }
