885eeac4e83331975ef3333b856e4687b8330112
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-08-08 18:55:58 +0530

    Time: 3 ms (99.23%), Space: 43.7 MB (76.55%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='i')
+                sb.append(s.charAt(i));
+            else
+                sb=sb.reverse();
+        }
+        return sb.toString();
+    }
+}
