50c01b4ba2e882719d60c24085425f1b22637c8d
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-08-09 23:56:53 +0530

    Time: 2 ms (79.50%), Space: 44.1 MB (79.11%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reach=0;
+        for(int i=0;i<nums.length;i++){
+           if(i>reach)
+               return false;
+            reach = Math.max(reach,i+nums[i]);
+        }
+        return true;
+    }
+}
