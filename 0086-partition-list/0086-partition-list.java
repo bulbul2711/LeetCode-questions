bac9aaf61321fd52e831c2bb9248628b02f965f2
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-08-15 14:29:37 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (36.82%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -12,8 +12,7 @@ class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode dummy1=new ListNode(0);
         ListNode dummy2=new ListNode(0);
-        ListNode tail1=dummy1;
-        ListNode tail2=dummy2;
+        ListNode tail1=dummy1,tail2=dummy2;
         ListNode curr=head;
         while(curr!=null){
             if(curr.val<x){
@@ -25,7 +24,6 @@ class Solution {
                 tail2=tail2.next;
             }
             curr=curr.next;
-            
         }
         tail1.next=dummy2.next;
         tail2.next=null;
