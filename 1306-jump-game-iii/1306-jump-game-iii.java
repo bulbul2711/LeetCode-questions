096ef1a80afe797eac5ea091803968f09b7cc86b
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-08-19 18:03:43 +0530

    Time: 11 ms (15.26%), Space: 54.1 MB (73.59%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        Queue<Integer> q=new LinkedList<>();
+        Queue<Integer> q=new ArrayDeque<>();
         Set<Integer> visited=new HashSet<>();
         q.add(start);
         visited.add(start);
