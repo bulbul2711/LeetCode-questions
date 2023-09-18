bfeb95e92a498efd56d9c2a43044aa5c243b6fef
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-09-18 23:29:44 +0530

    Time: 4 ms (15.72%), Space: 44.6 MB (59.35%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-hidden-flex-wrap-wrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondiv-styletransform-rotate180deg-svg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-hidden-flex-wrap-wrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondiv-styletransform-rotate180deg-svg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+       int[][] a=new int[mat.length][2];
+        for(int i=0;i<mat.length;i++){
+            int c=0;
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==1)
+                    c++;
+            }
+            a[i][0]=i;
+            a[i][1]=c;
+        }
+        Arrays.sort(a, Comparator.comparingInt(x -> x[1]));
+        int[] res=new int[k];
+        for(int i=0;i<k;i++)
+            res[i]=a[i][0];
+        return res;
+    }
+}
+
