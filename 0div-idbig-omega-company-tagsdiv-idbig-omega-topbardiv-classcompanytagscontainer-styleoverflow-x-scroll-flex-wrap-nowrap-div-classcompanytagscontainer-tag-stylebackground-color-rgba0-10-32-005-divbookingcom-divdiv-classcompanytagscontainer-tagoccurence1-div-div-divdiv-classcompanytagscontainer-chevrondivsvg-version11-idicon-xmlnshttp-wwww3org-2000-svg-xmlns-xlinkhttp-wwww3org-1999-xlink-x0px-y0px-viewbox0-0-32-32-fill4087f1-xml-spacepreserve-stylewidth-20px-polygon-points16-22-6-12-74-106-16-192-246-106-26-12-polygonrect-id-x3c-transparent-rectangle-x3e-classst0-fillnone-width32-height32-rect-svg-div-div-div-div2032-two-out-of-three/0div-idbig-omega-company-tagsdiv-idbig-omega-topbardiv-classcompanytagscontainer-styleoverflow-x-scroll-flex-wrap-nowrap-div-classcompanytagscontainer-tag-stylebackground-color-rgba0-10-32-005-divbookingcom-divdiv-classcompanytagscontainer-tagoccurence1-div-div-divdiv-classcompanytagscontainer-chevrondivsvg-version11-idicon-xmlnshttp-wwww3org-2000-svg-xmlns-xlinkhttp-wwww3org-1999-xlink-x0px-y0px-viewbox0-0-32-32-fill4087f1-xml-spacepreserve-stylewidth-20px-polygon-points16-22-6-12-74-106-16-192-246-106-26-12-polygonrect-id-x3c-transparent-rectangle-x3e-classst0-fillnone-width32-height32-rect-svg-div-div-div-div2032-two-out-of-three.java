42b6c9a17fc9adaa6dd99abee84b7406e2ff8a3f
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-09-23 23:29:45 +0530

    Time: 1 ms (100.00%), Space: 43.9 MB (52.39%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbookingcom-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2032-two-out-of-three/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbookingcom-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2032-two-out-of-three.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbookingcom-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2032-two-out-of-three/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbookingcom-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2032-two-out-of-three.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
-        List<Integer> a1=new ArrayList<>();
-        List<Integer> a2=new ArrayList<>();
         List<Integer> res=new ArrayList<>();
+        boolean a[]=new boolean[101];
+         boolean b[]=new boolean[101];
+         boolean c[]=new boolean[101];
+        
+        for(int i=0;i<nums1.length;i++)
+            a[nums1[i]]=true;
         for(int i=0;i<nums2.length;i++)
-            a1.add(nums2[i]);
+            b[nums2[i]]=true;
         for(int i=0;i<nums3.length;i++)
-            a2.add(nums3[i]);
-        for(int i=0;i<nums1.length;i++){
-            if(!res.contains(nums1[i]) && (a1.contains(nums1[i]) || a2.contains(nums1[i])))
-               res.add(nums1[i]);
-        }
-        for(int i=0;i<a2.size();i++){
-            int x=a2.get(i);
-            if(!res.contains(x) && a1.contains(x))
-                res.add(x);
+            c[nums3[i]]=true;
+        
+        for(int i=0;i<101;i++){
+            if((a[i] || b[i]) && (b[i] || c[i]) && (c[i] || a[i]))
+                res.add(i);
         }
         return res;
     }
