3278c09b907d2cab435bbcf3af60ede168f55270
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-09-23 22:27:15 +0530

    Time: 4 ms (15.65%), Space: 45.6 MB (6.11%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div942-di-string-match/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div942-di-string-match.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n=s.length();
+        int res[]=new int[n+1];
+        int a=0,b=n;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='I')
+                res[i]=a++;
+            else if(s.charAt(i)=='D')
+                res[i]=b--;
+        }
+        res[res.length-1]=a;
+        return res;
+    }
+}
