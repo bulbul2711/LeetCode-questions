b67ca6931964750b72622dec2c524d8ef235b001
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-09-25 23:57:54 +0530

    Time: 6 ms (12.77%), Space: 44.7 MB (9.86%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2570-merge-two-2d-arrays-by-summing-values/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2570-merge-two-2d-arrays-by-summing-values.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        int i=0,j=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        while(i<nums1.length && j<nums2.length){
+            int x=nums1[i][0];
+            int y=nums2[j][0];
+            if(x<y){
+                map.put(x,nums1[i][1]);
+                i++;
+            }
+            else if(y<x){
+                map.put(y,nums2[j][1]);
+                j++;
+            }
+            else{
+                map.put(x,nums1[i][1]+nums2[j][1]);
+                i++;
+                j++;
+            }
+        }
+        while(i<nums1.length){
+            map.put(nums1[i][0],nums1[i][1]);
+            i++;
+        }
+        while(j<nums2.length){
+            map.put(nums2[j][0],nums2[j][1]);
+            j++;
+        }
+        int res[][] = new int[map.size()][2];
+        int a=0;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            res[a][0]=entry.getKey();
+            res[a][1]=entry.getValue();
+            a++;
+        }
+        Arrays.sort(res,Comparator.comparingInt(x->x[0]));
+        return res;
+    }
+}
