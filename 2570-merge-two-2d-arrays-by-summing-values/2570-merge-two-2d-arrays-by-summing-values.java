4d32d5dc94bde4a9351e75b3929e72e4473f4bc6
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-09-26 00:20:28 +0530

    Time: 4 ms (43.71%), Space: 44.3 MB (72.15%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -1,32 +1,10 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        int i=0,j=0;
-        Map<Integer,Integer> map = new HashMap<>();
-        while(i<nums1.length && j<nums2.length){
-            int x=nums1[i][0];
-            int y=nums2[j][0];
-            if(x<y){
-                map.put(x,nums1[i][1]);
-                i++;
-            }
-            else if(y<x){
-                map.put(y,nums2[j][1]);
-                j++;
-            }
-            else{
-                map.put(x,nums1[i][1]+nums2[j][1]);
-                i++;
-                j++;
-            }
-        }
-        while(i<nums1.length){
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int i=0;i<nums1.length;i++)
             map.put(nums1[i][0],nums1[i][1]);
-            i++;
-        }
-        while(j<nums2.length){
-            map.put(nums2[j][0],nums2[j][1]);
-            j++;
-        }
+        for(int i=0;i<nums2.length;i++)
+            map.put(nums2[i][0],map.getOrDefault(nums2[i][0],0)+nums2[i][1]);
         int res[][] = new int[map.size()][2];
         int a=0;
         for(Map.Entry<Integer,Integer> entry : map.entrySet()){
@@ -34,7 +12,6 @@ class Solution {
             res[a][1]=entry.getValue();
             a++;
         }
-        Arrays.sort(res,Comparator.comparingInt(x->x[0]));
         return res;
     }
 }
