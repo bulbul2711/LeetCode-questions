1745adb0e5f55693a6f893bb990e304efce8b243
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-09-26 23:01:32 +0530

    Time: 4 ms (40.32%), Space: 41.4 MB (40.66%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,18 +1,19 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
+        
+        //monotonic stack
         HashMap<Character,Integer> map=new HashMap<>();
-        ArrayDeque<Character> st=new ArrayDeque<>();
         for(int i=0;i<s.length();i++){
             map.put(s.charAt(i),i);
         }
-        st.push(s.charAt(0));
+        ArrayDeque<Character> st=new ArrayDeque<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(!st.contains(ch)){
                while(!st.isEmpty() && st.peek()>ch && map.get(st.peek())>i){
                     st.pop();
-                    }
-            st.push(ch);
+               }
+               st.push(ch);
             }
         }
         StringBuffer sb=new StringBuffer();
