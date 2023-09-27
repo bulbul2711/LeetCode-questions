9b2037ae5f74b0f4647b67d88ecd9912adb2eb08
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-09-27 17:50:10 +0530

    Time: 5 ms (88.90%), Space: 43.1 MB (54.11%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divjane-street-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2085-count-common-words-with-one-occurrence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divjane-street-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2085-count-common-words-with-one-occurrence.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divjane-street-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2085-count-common-words-with-one-occurrence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divjane-street-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2085-count-common-words-with-one-occurrence.java
@@ -1,14 +1,18 @@
 class Solution {
     public int countWords(String[] a, String[] b) {
         Map<String,Integer> map1=new HashMap<>();
-         Map<String,Integer> map2=new HashMap<>();
+      //   Map<String,Integer> map2=new HashMap<>();
         int c=0;
         for(int i=0;i<a.length;i++)
             map1.put(a[i],map1.getOrDefault(a[i],0)+1);
-        for(int i=0;i<b.length;i++)
-            map2.put(b[i],map2.getOrDefault(b[i],0)+1);
-        for(String x:a){
-            if(map1.get(x)==1 && map2.getOrDefault(x,0)==1)
+        // for(int i=0;i<b.length;i++)
+        //     map2.put(b[i],map2.getOrDefault(b[i],0)+1);
+        for(String x:b){
+            if(map1.containsKey(x) && map1.get(x)<=1)
+                map1.put(x,map1.get(x)-1);
+        }
+        for(int i:map1.values()){
+            if(i==0)
                 c++;
         }
         return c;
