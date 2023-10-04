37b90bd2dbc4215b154f1a972d3df0fd14e370f0
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-10-04 22:59:22 +0530

    Time: 15 ms (83.30%), Space: 46.7 MB (81.07%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divwalmart-global-tech-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div706-design-hashmap/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoldman-sachs-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divwalmart-global-tech-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div706-design-hashmap.java
@@ -0,0 +1,81 @@
+class ListNode{
+    int pair[];
+    ListNode next;
+    ListNode(int key,int val){
+        pair=new int[]{key,val};
+        next=null;
+    }
+}
+class MyHashMap {
+     int m=1000;
+     ArrayList<ListNode> h;
+    public MyHashMap() {
+        h=new ArrayList<>();
+        for(int i=0;i<m;i++)
+            h.add(null);
+    }
+    
+    public void put(int key, int value) {
+        int idx=key%m;
+        if(h.get(idx)==null){
+            h.set(idx,new ListNode(key,value));
+        }
+        else{
+            ListNode curr=h.get(idx);
+            ListNode prev=null;
+            while(curr!=null){
+                if(curr.pair[0]==key){
+                    curr.pair[1]=value;
+                    return;
+                }
+                prev=curr;
+                curr=curr.next;
+            }
+            prev.next=new ListNode(key,value);
+        }
+    }
+    
+    public int get(int key) {
+        int idx=key%m;
+        if(h.get(idx)!=null){
+            ListNode curr=h.get(idx);
+            while(curr!=null){
+                if(curr.pair[0]==key)
+                    return curr.pair[1];
+                curr=curr.next;
+            }
+        }
+        return -1;
+    }
+    
+    public void remove(int key) {
+        int idx=key%m;
+        ListNode curr=h.get(idx);
+        if(curr==null)
+            return;
+        else if(curr.pair[0]==key){
+            h.set(idx,curr.next);
+            curr.next=null;
+        }
+        else{
+            ListNode prev=curr;
+            curr=curr.next;
+            while(curr!=null){
+                if(curr.pair[0]==key){
+                  prev.next=curr.next;
+                  break;
+                }
+                prev=curr;
+                curr=curr.next;
+            }
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
