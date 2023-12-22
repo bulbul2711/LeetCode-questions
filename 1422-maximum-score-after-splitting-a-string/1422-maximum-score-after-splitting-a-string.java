a27deede8a176a9c35e9765507f33c7e59d4388f
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2023-12-23 01:21:28 +0530

    Time: 43 ms (7.39%), Space: 45.2 MB (6.09%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxScore(String s) {
+        int max=Integer.MIN_VALUE;
+        int sum1=0;
+        String l="",r="";
+        for(int i=0;i<s.length()-1;i++){
+            l=s.substring(0,i+1);
+            r=s.substring(i+1);
+            sum1=sumleft(l)+sumright(r);
+            System.out.println(sumleft(l)+" "+sumright(r));
+            max=Math.max(max,sum1);
+        }
+        return max;
+    }
+    int sumleft(String str){
+        int c1=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='0')
+                c1++;
+        }
+        return c1;
+    }
+    int sumright(String str){
+        int c2=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='1')
+                c2++;
+        }
+        return c2;
+    }
+}
