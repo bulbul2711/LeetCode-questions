bf9eb634c713bbbac88a8e042b112be555358aa1
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-01-13 20:04:24 +0530

    Time: 10 ms (5.38%), Space: 84.3 MB (5.05%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,20 +15,40 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<inorder.length;i++)
-            map.put(inorder[i],i);
-        TreeNode root=build(inorder,0,inorder.length-1, postorder,0,postorder.length-1, map);
+    //     Map<Integer,Integer> map=new HashMap<>();
+    //     for(int i=0;i<inorder.length;i++)
+    //         map.put(inorder[i],i);
+    //     TreeNode root=build(inorder,0,inorder.length-1, postorder,0,postorder.length-1, map);
+    //     return root;
+    // }
+    // TreeNode build(int[] inorder,int inS,int inE, int[] postorder,int poS,int poE, Map<Integer,Integer> map){
+    //     if(inS>inE || poS>poE)
+    //         return null;
+    //     TreeNode root=new TreeNode(postorder[poE]);
+    //     int inroot=map.get(postorder[poE]);
+    //     int numsLeft=inroot-inS;
+    //     root.left = build(inorder,inS,inroot-1, postorder,poS,poS+numsLeft-1, map);
+    //     root.right=build(inorder,inroot+1,inE, postorder,poS+numsLeft,poE-1, map);
+    //     return root;
+        
+          if (postorder.length == 0 || inorder.length == 0) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(postorder[postorder.length-1]);
+        int mid = findIndex(inorder, postorder[postorder.length-1]);
+        root.left = buildTree(Arrays.copyOfRange(inorder, 0, mid),
+                              Arrays.copyOfRange(postorder, 0, mid));
+        root.right = buildTree(Arrays.copyOfRange(inorder, mid + 1, inorder.length),Arrays.copyOfRange(postorder, mid, postorder.length-1));
+
         return root;
     }
-    TreeNode build(int[] inorder,int inS,int inE, int[] postorder,int poS,int poE, Map<Integer,Integer> map){
-        if(inS>inE || poS>poE)
-            return null;
-        TreeNode root=new TreeNode(postorder[poE]);
-        int inroot=map.get(postorder[poE]);
-        int numsLeft=inroot-inS;
-        root.left = build(inorder,inS,inroot-1, postorder,poS,poS+numsLeft-1, map);
-        root.right=build(inorder,inroot+1,inE, postorder,poS+numsLeft,poE-1, map);
-        return root;
+
+    private int findIndex(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target)
+                return i;
+        }
+        return -1;
     }
 }
