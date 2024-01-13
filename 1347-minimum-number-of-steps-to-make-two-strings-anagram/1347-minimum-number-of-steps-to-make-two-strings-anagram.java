ae8e02edda5b29f51425d4feaa66d76dd63d70d5
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-01-14 01:38:48 +0530

    Time: 8 ms (76.50%), Space: 45 MB (38.12%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,18 +1,31 @@
 class Solution {
     public int minSteps(String s, String t) {
-        int sum=0;
-       HashMap<Character,Integer> map=new HashMap<>();
-       // HashMap<Character,Integer> map2=new HashMap<>();
-        for(int i=0;i<s.length();i++)
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+         int sum=0;
+       // HashMap<Character,Integer> map=new HashMap<>();
+       //  for(int i=0;i<s.length();i++)
+       //      map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+       //  for(int i=0;i<t.length();i++){
+       //      char ch=t.charAt(i);
+       //      if(map.containsKey(ch) && map.get(ch)!=0)
+       //          map.put(ch,map.get(ch)-1);
+       //  }
+       //   for(Map.Entry<Character,Integer> entry:map.entrySet()){
+       //       sum+=entry.getValue();
+       //   }
+       //  return sum;
+        
+        int[] a=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            a[ch-'a']++;
+        }
         for(int i=0;i<t.length();i++){
             char ch=t.charAt(i);
-            if(map.containsKey(ch) && map.get(ch)!=0)
-                map.put(ch,map.get(ch)-1);
+            if(a[ch-'a']!=0)
+                a[ch-'a']--;
         }
-         for(Map.Entry<Character,Integer> entry:map.entrySet()){
-             sum+=entry.getValue();
-         }
+        for(int i=0;i<26;i++)
+            sum+=a[i];
         return sum;
     }
 }
