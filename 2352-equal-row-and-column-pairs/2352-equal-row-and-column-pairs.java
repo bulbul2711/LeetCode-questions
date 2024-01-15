f215e38ac93dd4e2f82c686895850625cc7966dc
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-01-15 19:49:28 +0530

    Time: 39 ms (47.45%), Space: 51.5 MB (40.86%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -2,30 +2,23 @@ class Solution {
     public int equalPairs(int[][] grid) {
        
         HashMap<String,Integer> map=new HashMap<>();
-        int c=0;
-         
+        int c=0; 
         for(int i=0;i<grid.length;i++){
-            StringBuffer sb=new StringBuffer();
+            StringBuilder sb=new StringBuilder();
             for(int j=0;j<grid.length;j++){
                 sb.append(grid[i][j]);
                  sb.append(",");
             }
-           
             map.put(sb.toString(),map.getOrDefault(sb.toString(),0)+1);
-        
         }
-        
         for(int k=0;k<grid.length;k++){
-            StringBuffer sb1=new StringBuffer();
+            StringBuilder sb1=new StringBuilder();
             for(int l=0;l<grid.length;l++){
                 sb1.append(grid[l][k]);
                  sb1.append(",");
             }
-             
             if(map.containsKey(sb1.toString()))
                 c+=map.get(sb1.toString());
-            
-            
         }
         return c;
     }
