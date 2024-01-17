658051a58f54b2ca8b3155fc09058d57ebb4fafb
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-01-17 11:07:19 +0530

    Time: 2 ms (98.10%), Space: 41.7 MB (21.99%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         Map<Integer,Integer> map=new HashMap<>();
-        Set<Integer> hs=new HashSet<>();
+      //  Set<Integer> hs=new HashSet<>();
         for(int i=0;i<arr.length;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            hs.add(entry.getValue());
-        }
+        // for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+        //     hs.add(entry.getValue());
+        
+    HashSet<Integer> hs=new HashSet<>(map.values());
         return map.size()==hs.size();
     }
 }
