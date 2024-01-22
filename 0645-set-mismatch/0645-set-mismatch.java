196c81a09955a95cc2d5e90b6a766bd132a2cc61
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-01-22 20:04:13 +0530

    Time: 2 ms (97.74%), Space: 46.2 MB (8.03%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,37 +1,19 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        // int[] ans=new int[2];
-        // int[] a=new int[10001];
-        // for(int i=0;i<nums.length;i++){
-        //     if(a[nums[i]]==0)
-        //         a[nums[i]]=-1;
-        //     else
-        //         a[nums[i]]*=-1;
-        // }
-        // for(int i=1;i<=nums.length;i++){
-        //     if(a[i]==1)
-        //         ans[0]=i;
-        //     if(a[i]==0)
-        //         ans[1]=i;
-        // }
-        // return ans;
-        
-        int dup = -1, missing = -1;
-        
-        for (int i = 1; i <= nums.length; i++) {
-            int count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == i) {
-                    count++;
-                }
-            }
-            if (count == 2) {
-                dup = i;
-            } else if (count == 0) {
-                missing = i;
-            }
+        int[] ans=new int[2];
+        int[] a=new int[10001];
+        for(int i=0;i<nums.length;i++){
+            if(a[nums[i]]==0)
+                a[nums[i]]=-1;
+            else
+                a[nums[i]]*=-1;
         }
-        
-        return new int[] {dup, missing};
+        for(int i=1;i<=nums.length;i++){
+            if(a[i]==1)
+                ans[0]=i;
+            if(a[i]==0)
+                ans[1]=i;
+        }
+        return ans;
     }
 }
