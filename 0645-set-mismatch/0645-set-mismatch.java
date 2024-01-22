9cd32e166c1a1eed4ebbea223dfdee4efd852678
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-01-22 19:58:54 +0530

    Time: 2 ms (97.74%), Space: 46 MB (8.03%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] ans=new int[2];
+        int[] a=new int[10001];
+        for(int i=0;i<nums.length;i++){
+            if(a[nums[i]]==0)
+                a[nums[i]]=-1;
+            else
+                a[nums[i]]*=-1;
+        }
+        for(int i=1;i<=nums.length;i++){
+            if(a[i]==1)
+                ans[0]=i;
+            if(a[i]==0)
+                ans[1]=i;
+        }
+        return ans;
+    }
+}
