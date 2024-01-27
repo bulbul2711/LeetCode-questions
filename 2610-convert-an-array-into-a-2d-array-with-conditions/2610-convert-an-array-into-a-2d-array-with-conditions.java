ac03308bcb631a711beebee9bee7f4d553615e0a
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-01-27 11:00:27 +0530

    Time: 1 ms (100.00%), Space: 45.2 MB (11.53%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,25 +1,36 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        int a=0;
+        // List<List<Integer>> res=new ArrayList<>();
+        // int a=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(res.size()==0){
+        //         List<Integer> list=new ArrayList<>();
+        //         list.add(nums[i]);
+        //     }
+        //     for(int j=0;j<res.size();j++){
+        //         if(!res.get(j).contains(nums[i])){
+        //             res.get(j).add(nums[i]);
+        //             a=-1;
+        //             break;
+        //         }
+        //     }
+        //     if(a==0){
+        //         List<Integer> l=new ArrayList<>();
+        //         l.add(nums[i]);
+        //         res.add(l);
+        //     }
+        //     a=0;  
+        // }
+        // return res;
+         List<List<Integer>> res=new ArrayList<>();
+        int n=nums.length;
+        int f[]=new int[n+1];
         for(int i=0;i<nums.length;i++){
-            if(res.size()==0){
-                List<Integer> list=new ArrayList<>();
-                list.add(nums[i]);
-            }
-            for(int j=0;j<res.size();j++){
-                if(!res.get(j).contains(nums[i])){
-                    res.get(j).add(nums[i]);
-                    a=-1;
-                    break;
-                }
-            }
-            if(a==0){
-                List<Integer> l=new ArrayList<>();
-                l.add(nums[i]);
-                res.add(l);
-            }
-            a=0;  
+            int x=nums[i];
+            if(f[x]>=res.size())
+                res.add(new ArrayList<>());
+            res.get(f[x]).add(x);
+            f[x]++;
         }
         return res;
     }
