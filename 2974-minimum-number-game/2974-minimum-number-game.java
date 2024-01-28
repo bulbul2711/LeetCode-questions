99bea657cfdf27e89ea6df304afcdde29c5aa906
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-01-28 11:40:51 +0530

    Time: 2 ms (98.80%), Space: 44.8 MB (57.41%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,31 +1,12 @@
-// class Solution {
-//     public int[] numberGame(int[] nums) {
-//     //     Arrays.sort(nums);
-    //     for(int i=0;i<nums.length-1;i+=2){
-    //         int t=nums[i];
-    //         nums[i]=nums[i+1];
-    //         nums[i+1]=t;
-    //     }
-    //     return nums;
-    // }
-        class Solution {
+class Solution {
     public int[] numberGame(int[] nums) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        int ans[]=new int[nums.length];
-        for(int i:nums)
-        {
-            pq.offer(i);
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i+=2){
+            int t=nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=t;
         }
-        int ind=0;
-        while(pq.size()!=0)
-        {
-            int a=pq.poll();
-            int b=pq.poll();
-            ans[ind++]=b;
-            ans[ind++]=a;
-        }
-        return ans;
+        return nums;
     }
-        
-
 }
+        
