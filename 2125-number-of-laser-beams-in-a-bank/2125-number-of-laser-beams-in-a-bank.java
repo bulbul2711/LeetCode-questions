7c0f06f2de5b22a790cb7e39acc10a8b8609342b
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-01-30 22:11:23 +0530

    Time: 12 ms (67.51%), Space: 45.3 MB (23.29%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,30 +1,17 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int sum=0;int l=0,r=0;
-        while(l<bank.length && r<bank.length){
-            while(l<bank.length && bank[l].indexOf('1')==-1)
-                l++;
-            r=l+1;
-            while(r<bank.length && bank[r].indexOf('1')==-1)
-                r++;
-            if(r<bank.length && l<r)
-                sum+=count(bank[l],bank[r]);
-            l++;
-            r++;
-        }
-        return sum;
-    }
-    int count(String s1, String s2){
-        int c1=0,c2=0;
-        for(int i=0;i<s1.length();i++){
-            if(s1.charAt(i)=='1')
-                c1++;
-        }
-        for(int i=0;i<s2.length();i++){
-            if(s2.charAt(i)=='1')
-                c2++;
-        }
-    //System.out.println(c1*c2);
-        return c1*c2;
+        int prev=0,ans=0;
+       for(String s : bank){
+           int count=0;
+           for(int i=0;i<s.length();i++){
+               if(s.charAt(i)=='1')
+                    count++;
+           }
+           if(count>0){
+               ans+=prev*count;
+               prev=count;
+           }
+       }
+        return ans;
     }
 }
