cda026893355d9486d8b714c1bddea6b2ef2f9cf
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-02-05 20:04:25 +0530

    Time: 1 ms (100.00%), Space: 44.6 MB (65.20%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,22 +1,43 @@
-class Solution {
-    public int firstUniqChar(String s) {
-        // int[] a=new int[26];
-        // for(int i=0;i<s.length();i++){
-        //     char ch=s.charAt(i);
-        //     a[ch-'a']++;
-        // }
-        // for(int i=0;i<s.length();i++){
-        //     char ch=s.charAt(i);
-        //     if(a[ch-'a']==1)
-        //         return i;
-        // }
-        // return -1;
+// class Solution {
+//     public int firstUniqChar(String s) {
+//         // int[] a=new int[26];
+//         // for(int i=0;i<s.length();i++){
+//         //     char ch=s.charAt(i);
+//         //     a[ch-'a']++;
+//         // }
+//         // for(int i=0;i<s.length();i++){
+//         //     char ch=s.charAt(i);
+//         //     if(a[ch-'a']==1)
+//         //         return i;
+//         // }
+//         // return -1;
         
-         for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-             if(s.indexOf(ch)==s.lastIndexOf(ch))
-                 return i;
-         }
-        return -1;
-    }
+//          for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//              if(s.indexOf(ch)==s.lastIndexOf(ch))
+//                  return i;
+//          }
+//         return -1;
+//     }
+// }
+
+class Solution {
+     public int firstUniqChar(String s) {
+        // Stores lowest index / first index
+        int ans = Integer.MAX_VALUE;
+        // Iterate from a to z which is 26 which makes it constant
+        for(char c='a'; c<='z';c++){
+            // indexOf will return first index of alphabet and lastIndexOf will return last index
+            // if both are equal then it has occured only once.
+            // through this we will get all index's which are occured once
+            // but our answer is lowest index
+            int index = s.indexOf(c);
+            if(index!=-1&&index==s.lastIndexOf(c)){
+                ans = Math.min(ans,index);
+            }
+        }
+
+        // If ans remain's Integer.MAX_VALUE then their is no unique character
+        return ans==Integer.MAX_VALUE?-1:ans;
+    }   
 }
