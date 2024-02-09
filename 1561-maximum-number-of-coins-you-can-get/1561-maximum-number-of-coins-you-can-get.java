a2b71fb071a49acf4897b03d3684d9c6aacd8816
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-02-09 16:36:36 +0530

    Time: 3 ms (99.31%), Space: 55.1 MB (89.32%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -1,12 +1,52 @@
+// class Solution {
+//     public int maxCoins(int[] piles) {
+//         Arrays.sort(piles);
+//         int res=0;
+//         int x=piles.length-2;
+//         for(int i=0;i<piles.length/3;i++){
+//             res+=piles[x];
+//             x-=2;
+//         }
+//         return res;
+//     }
+// }
 class Solution {
     public int maxCoins(int[] piles) {
-        Arrays.sort(piles);
-        int res=0;
-        int x=piles.length-2;
-        for(int i=0;i<piles.length/3;i++){
-            res+=piles[x];
-            x-=2;
+        int max = 0;
+        int n = piles.length;
+
+        for(int i : piles)
+        {
+            if(max < i) max = i;
         }
-        return res;
+
+        int[] freq = new int[max+1];
+
+        for(int i : piles)
+        {
+            freq[i]++;
+        }
+        
+        int coins = 0;
+        int chance = n / 3;
+
+        int turn = 1;
+        int i = max;
+
+        while(chance != 0){
+            if(freq[i] > 0){
+                if(turn == 1) turn = 0;
+                else {
+                    chance--;
+                    turn = 1;
+                    coins += i;
+                }
+                freq[i]--;
+            }else{
+                i--;
+            }
+        }
+
+        return coins;
     }
 }
