f1aa8ea89fe267b5e51f4afaa01880f69a8023da
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-03-08 00:21:50 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (72.05%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,41 +1,16 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        // List<String> ans=new ArrayList<>();
-        // List<Integer> list=new ArrayList<>();
-        // Stack<Integer> st=new Stack<>();
-        // for(int i=0;i<target.length;i++){
-        //     list.add(target[i]);
-        // }
-        // for(int i=1;i<=n;i++){
-        //     ans.add("Push");
-        //     st.add(i);
-        //     while(st.size()!=0 && !list.contains(st.peek())){
-        //         st.pop();
-        //         ans.add("Pop");                
-        //     }
-        //     if(st.size()==target.length)
-        //         return ans;
-        // }
-        // return ans;
-        
-        List<String> result = new ArrayList<>();
-        int current = 1; // The current number to be pushed.
-
-        for (int i = 0; i < target.length; i++) {
-            while (current < target[i]) {
-                // While the current number is less than the target number,
-                // push the current number and generate the "Push" operation.
-                result.add("Push");
-                result.add("Pop"); // After pushing, immediately pop.
-                current++;
+        List<String> res=new ArrayList<>();
+        int curr=1;
+        for(int i=0;i<target.length;i++){
+            while(curr<target[i]){
+                res.add("Push");
+                res.add("Pop");
+                curr++;
             }
-
-            // The current number matches the target number, so push it.
-            result.add("Push");
-
-            current++; // Move to the next number to be pushed.
+            res.add("Push");
+            curr++;
         }
-
-        return result;
+        return res;
     }
 }
