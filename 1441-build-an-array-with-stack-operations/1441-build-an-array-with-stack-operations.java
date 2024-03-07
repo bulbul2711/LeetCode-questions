2b2489d0292929823b2d17ccbd21444fde693067
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-03-08 00:13:33 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (93.54%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,22 +1,41 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        List<String> ans=new ArrayList<>();
-        List<Integer> list=new ArrayList<>();
-        Stack<Integer> st=new Stack<>();
-        for(int i=0;i<target.length;i++){
-            list.add(target[i]);
-        }
-      
-        for(int i=1;i<=n;i++){
-            ans.add("Push");
-            st.add(i);
-            while(st.size()!=0 && !list.contains(st.peek())){
-                st.pop();
-                ans.add("Pop");                
+        // List<String> ans=new ArrayList<>();
+        // List<Integer> list=new ArrayList<>();
+        // Stack<Integer> st=new Stack<>();
+        // for(int i=0;i<target.length;i++){
+        //     list.add(target[i]);
+        // }
+        // for(int i=1;i<=n;i++){
+        //     ans.add("Push");
+        //     st.add(i);
+        //     while(st.size()!=0 && !list.contains(st.peek())){
+        //         st.pop();
+        //         ans.add("Pop");                
+        //     }
+        //     if(st.size()==target.length)
+        //         return ans;
+        // }
+        // return ans;
+        
+        List<String> result = new ArrayList<>();
+        int current = 1; // The current number to be pushed.
+
+        for (int i = 0; i < target.length; i++) {
+            while (current < target[i]) {
+                // While the current number is less than the target number,
+                // push the current number and generate the "Push" operation.
+                result.add("Push");
+                result.add("Pop"); // After pushing, immediately pop.
+                current++;
             }
-            if(st.size()==target.length)
-                return ans;
+
+            // The current number matches the target number, so push it.
+            result.add("Push");
+
+            current++; // Move to the next number to be pushed.
         }
-        return ans;
+
+        return result;
     }
 }
