5fa448192a7ead884324728cd64fa5f3c3c0250d
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-05-09 02:40:35 +0530

    Time: 7 ms (93.95%), Space: 45.8 MB (17.06%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,21 +1,27 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n=score.length;
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<n;i++)
-            list.add(score[i]);
-        Arrays.sort(score);
-        String[] ans=new String[n];
+        int sorted[]=score.clone();
+        Arrays.sort(sorted);
+        int rank=4;
+        Map<Integer, String> map=new HashMap<>();
         for(int i=n-1;i>=0;i--){
-            int x=list.indexOf(score[i]);
-            if(i==n-1)
-                ans[x]="Gold Medal";
-            else if(i==n-2)
-                ans[x]="Silver Medal";
-            else if(i==n-3)
-                ans[x]="Bronze Medal";
-            else
-                ans[x]=Integer.toString(n-i);
+            if(i==n-1){
+                map.put(sorted[i], "Gold Medal");
+            }
+            else if(i==n-2){
+                map.put(sorted[i], "Silver Medal");
+            }
+            else if(i==n-3){
+                map.put(sorted[i], "Bronze Medal");
+            }
+            else{
+                map.put(sorted[i], String.valueOf(rank++));
+            }
+        }
+        String ans[]=new String[n];
+        for(int i=0;i<n;i++){
+            ans[i]=map.get(score[i]);
         }
         return ans;
     }
