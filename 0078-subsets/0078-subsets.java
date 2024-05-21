64a198eee3a3069a6cfa9829f8f6393c8abc7e27
bulbul2711 <121823138+bulbul2711@users.noreply.github.com>
2024-05-21 22:58:28 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (59.10%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,15 +2,15 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> ds = new ArrayList<>();
-        possible(nums,ds,res);
+        dfs(nums,ds,res);
         return res;
     }
-    void possible(int[] nums,List<Integer> ds,List<List<Integer>> res){
+    void dfs(int[] nums,List<Integer> ds,List<List<Integer>> res){
         res.add(ds);
         for(int i=0;i<nums.length;i++){
             List<Integer> al=new ArrayList<>(ds);
             al.add(nums[i]);
-            possible(Arrays.copyOfRange(nums,i+1,nums.length),al,res);
+            dfs(Arrays.copyOfRange(nums,i+1,nums.length),al,res);
         }
     }
 }
